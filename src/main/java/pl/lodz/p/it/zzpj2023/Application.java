601bf093dfba1eab6f9c7ba23659f9a5deb745d5
236618 <236618@edu.p.lodz.pl>
2023-05-17 18:05:04 +0200

    fixing bugs

--- src/main/java/pl/lodz/p/it/zzpj2023/Application.java
+++ src/main/java/pl/lodz/p/it/zzpj2023/Application.java
@@ -3,11 +3,10 @@ package pl.lodz.p.it.zzpj2023;
 import jakarta.annotation.security.DeclareRoles;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
 import pl.lodz.p.it.zzpj2023.roles.Roles;
 
 @ApplicationPath("/api")
 @DeclareRoles({Roles.ADMIN, Roles.CAPITAN, Roles.MANAGER, Roles.COACH, Roles.REFEREE})
 @ApplicationScoped
-public class SSBDApplication extends Application {
+public class Application extends jakarta.ws.rs.core.Application {
 }
